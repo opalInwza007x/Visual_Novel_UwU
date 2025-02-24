e2c1b6babef761c7db073b105fbc0d858f5270d4
Pattarapon Noonun <fluk3874@gmail.com>
2025-02-24 15:42:43 +0700

    add image while next pressed and upgrade text box

--- src/main/Chapter1.java
+++ src/main/Chapter1.java
@@ -1,3 +1,5 @@
+
+
 package main;
 
 import javafx.animation.KeyFrame;
@@ -14,6 +16,9 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.scene.media.Media;
@@ -22,36 +27,36 @@ import java.net.URL;
 
 public class Chapter1 {
 	private String[][] storyTexts = {
-	    {"เพื่อน", "เห้ย คเชน มีเรื่องไรป่าว เห็นนั่งเหม่ออยู่คนเดียวมาสักพักละ","whoosh"},
-	    {"คเชน", "อ๋อ ไม่มีไร ช่วงนี้เครียดเรื่องเงินว่ะ","pop"},
-	    {"เพื่อน", "ที่บ้านการเงินไม่ดีหรอเพื่อน ไม่เห็นเคยเล่าให้ฟังเลย","whoosh"},
-	    {"คเชน", "ป่าว พอดีอยากหาตังไปเติมซื้อ Minecraft ไอดีแท้น่ะ","pop"},
-	    {"เพื่อน", "...","whoosh"},
-	    {"เพื่อน", "ช่างเหอะ ช่วงนี้ได้ยินเรื่องเพื่อนผู้หญิงห้องข้าง ๆ เราป้ะ","pop"},
-	    {"คเชน", "ทำไม ? มีไรหรอ ?","whoosh"},
-	    {"เพื่อน", "เธอก็เป็นเพื่อนรุ่นเดียวกับเรานี่แหละแถมน่ารักดีด้วย แต่ติดตรงที่เธอมีบางอย่างแปลก ๆ นี่สิ","pop"},
-	    {"คเชน", "บางอย่างแปลก ๆ ?","whoosh"},
-	    {"เพื่อน", "เธอน่ะ ห่อเนื้อวัว Thai Wagyu แบบ medium rare มากินที่โรงเรียนทุกวันเลย","pop"},
-	    {"เพื่อน", "ไม่อยากจะคิดเลยว่าทำไมเธอไม่กินซอยจุ๊กันนะ ของขึ้นชื่อบ้านเราแท้ ๆ เพราะอย่างงี้ไงเธอถึงไม่มีเพื่อนคบซักคนอะ","whoosh"},
-	    {"คเชน", "อ๋อ… จำได้ละ แต่กินเนื้อดิบ ๆ มันก็ไม่ดีนะเว้ย อย่างน้อยก็ควรทำเนื้อให้สุกก่อนกินสิ เธอทำถูกแล้วล่ะ ถือว่าเป็นการปลูกฝังค่านิยมดี ๆ ให้รุ่นน้องในรร. ด้วยไง","pop"},
-	    {"คเชน", "Dev เกมนี้คิดบทมาดีจริง ๆ","wow"},
-	    {"เพื่อน", "พูดถึงใครอะ ?","pop"},
-	    {"คเชน", "เอาเถอะ... แต่จริง ๆ ที่เธอไม่มีเพื่อนอาจจะเป็นเพราะมีแต่คนแบบนายอยู่ในโรงเรียนก็ได้นะ","whoosh"},
-	    {"เพื่อน", ":ArchettoLOOKATYOU: :ArchettoLOOKATYOU:","pop"},
-	    {"เพื่อน", "เชื่อครับไม่โม้... แต่ฉันได้ยินมาว่าเธอก็พยายามหาเพื่อนอยู่นะ แต่ไม่มีใครอยากคุยกับเธอสักคนเลย","whoosh"},
-	    {"เพื่อน", "มีแต่คนบอกว่าเวลาอยู่กับเธอแล้วมันไม่สบายใจ","pop"},
-	    {"เพื่อน", "ฉันก็รู้สึกคล้ายกัน ใครจะอยู่ก็อยู่ ข้าไม่อยู่แล้ว บรรยากาศรอบตัวเธอมันเหมือนเหตุการณ์ในหนังผีที่เราไปดูมาเมื่อตอนนั้นเลย","whoosh"},
-	    {"คเชน", "เพ้อเจ้อ ดูธี่หยดแล้วหลอน","wow"},
-	    {"คเชน", "ไปนั่งที่เถอะ อาจารย์เข้าห้องมาแล้ว","whoosh"}
+	    {"normal","เพื่อน", "เห้ย คเชน มีเรื่องไรป่าว เห็นนั่งเหม่ออยู่คนเดียวมาสักพักละ","whoosh"},
+	    {"smile","คเชน", "อ๋อ ไม่มีไร ช่วงนี้เครียดเรื่องเงินว่ะ","pop"},
+	    {"normal","เพื่อน", "ที่บ้านการเงินไม่ดีหรอเพื่อน ไม่เห็นเคยเล่าให้ฟังเลย","whoosh"},
+	    {"normal","คเชน", "ป่าว พอดีอยากหาตังไปเติมซื้อ Minecraft ไอดีแท้น่ะ","pop"},
+	    {"normal","เพื่อน", "...","whoosh"},
+	    {"normal","เพื่อน", "ช่างเหอะ ช่วงนี้ได้ยินเรื่องเพื่อนผู้หญิงห้องข้าง ๆ เราป้ะ","pop"},
+	    {"smile","คเชน", "ทำไม ? มีไรหรอ ?","whoosh"},
+	    {"normal","เพื่อน", "เธอก็เป็นเพื่อนรุ่นเดียวกับเรานี่แหละแถมน่ารักดีด้วย แต่ติดตรงที่เธอมีบางอย่างแปลก ๆ นี่สิ","pop"},
+	    {"normal","คเชน", "บางอย่างแปลก ๆ ?","whoosh"},
+	    {"normal","เพื่อน", "เธอน่ะ ห่อเนื้อวัว Thai Wagyu แบบ medium rare มากินที่โรงเรียนทุกวันเลย","pop"},
+	    {"normal","เพื่อน", "ไม่อยากจะคิดเลยว่าทำไมเธอไม่กินซอยจุ๊กันนะ ของขึ้นชื่อบ้านเราแท้ ๆ เพราะอย่างงี้ไงเธอถึงไม่มีเพื่อนคบซักคนอะ","whoosh"},
+	    {"smile","คเชน", "อ๋อ… จำได้ละ แต่กินเนื้อดิบ ๆ มันก็ไม่ดีนะเว้ย อย่างน้อยก็ควรทำเนื้อให้สุกก่อนกินสิ เธอทำถูกแล้วล่ะ ถือว่าเป็นการปลูกฝังค่านิยมดี ๆ ให้รุ่นน้องในรร. ด้วยไง","pop"},
+	    {"normal","คเชน", "Dev เกมนี้คิดบทมาดีจริง ๆ","wow"},
+	    {"normal","เพื่อน", "พูดถึงใครอะ ?","pop"},
+	    {"normal","คเชน", "เอาเถอะ... แต่จริง ๆ ที่เธอไม่มีเพื่อนอาจจะเป็นเพราะมีแต่คนแบบนายอยู่ในโรงเรียนก็ได้นะ","whoosh"},
+	    {"normal","เพื่อน", ":ArchettoLOOKATYOU: :ArchettoLOOKATYOU:","pop"},
+	    {"normal","เพื่อน", "เชื่อครับไม่โม้... แต่ฉันได้ยินมาว่าเธอก็พยายามหาเพื่อนอยู่นะ แต่ไม่มีใครอยากคุยกับเธอสักคนเลย","whoosh"},
+	    {"normal","เพื่อน", "มีแต่คนบอกว่าเวลาอยู่กับเธอแล้วมันไม่สบายใจ","pop"},
+	    {"normal","เพื่อน", "ฉันก็รู้สึกคล้ายกัน ใครจะอยู่ก็อยู่ ข้าไม่อยู่แล้ว บรรยากาศรอบตัวเธอมันเหมือนเหตุการณ์ในหนังผีที่เราไปดูมาเมื่อตอนนั้นเลย","whoosh"},
+	    {"normal","คเชน", "เพ้อเจ้อ ดูธี่หยดแล้วหลอน","wow"},
+	    {"normal","คเชน", "ไปนั่งที่เถอะ อาจารย์เข้าห้องมาแล้ว","whoosh"}
 	};
-	
+
 	private int currentTextIndex = 0;
     private Timeline timeline;
     private ImageView friendImage;
     private ImageView cashenImage;	
     private MediaPlayer backgroundMusic;
     private MediaPlayer effectPlayer;
-    
+
     public void startChapter(Stage primaryStage) {
     	playBackgroundMusic();
     	loadSoundEffect();
@@ -60,27 +65,40 @@ public class Chapter1 {
         
        
         ImageView background = createImageView("/resources/classroomTest.jpg", 968, 486);
-        TextArea textBox = createTextArea();
+
+        TextFlow textBox = new TextFlow();
+        textBox.setPadding(new Insets(10));
+        textBox.setPrefHeight(162);
+        textBox.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-padding: 10px;");
+
         Button nextButton = createButton("Next", "rgba(255, 0, 0, 0.7)", 16);
+
         friendImage = createSpeakerImage("เพื่อน");
         cashenImage = createSpeakerImage("คเชน");
+
         updateSpeakerVisibility();
+
         nextButton.setOnAction(event -> handleNextText(primaryStage, textBox));
         
         StackPane textBoxWithButton = new StackPane(textBox, nextButton);
         StackPane.setAlignment(nextButton, Pos.BOTTOM_RIGHT);
+
         HBox speakerPane = new HBox(50, friendImage, cashenImage);
         speakerPane.setAlignment(Pos.BOTTOM_CENTER);
+
         StackPane stackPane = new StackPane(background, speakerPane);
+
         timeline = createTimeline(textBox);
         timeline.play();
+
         root.getChildren().addAll(stackPane, textBoxWithButton);
+
         primaryStage.setScene(new Scene(root, 968, 648, Color.BLACK));
     }
     
     private void playBackgroundMusic() {
         try {
-            URL resource = getClass().getResource("/resources/bgChap1.mp4");
+            URL resource = getClass().getResource("/resources/bgChap1.mp3");
             if (resource != null) {
                 Media media = new Media(resource.toExternalForm());
                 backgroundMusic = new MediaPlayer(media);
@@ -94,12 +112,12 @@ public class Chapter1 {
             e.printStackTrace();
         }
     }
-    
     private void loadSoundEffect() {
     	 try {
     	        URL whooshURL = getClass().getResource("/resources/whoosh.mp3");
-    	        URL popURL = getClass().getResource("/resources/pop.mp4");
-    	        URL wowURL = getClass().getResource("/resources/wow.mp4");
+    	        URL popURL = getClass().getResource("/resources/pop.mp3");
+    	        URL wowURL = getClass().getResource("/resources/wow.mp3");
+
     	        if (whooshURL != null && popURL != null &&  wowURL != null) {
     	            effectPlayer = new MediaPlayer(new Media(whooshURL.toExternalForm()));
     	        } else {
@@ -109,36 +127,39 @@ public class Chapter1 {
     	        e.printStackTrace();
     	    }
     }
-    
     private ImageView createImageView(String path, double width, double height) {
         ImageView imageView = new ImageView(new Image(getClass().getResource(path).toExternalForm()));
         imageView.setFitWidth(width);
         imageView.setFitHeight(height);
         return imageView;
     }
-    
+
     private Button createButton(String text, String color, int fontSize) {
         Button button = new Button(text);
         button.setStyle(String.format("-fx-background-color: %s; -fx-text-fill: white; -fx-font-size: %dpx;", color, fontSize));
         return button;
     }
-    
-    private TextArea createTextArea() {
-        TextArea textBox = new TextArea();
-        textBox.setEditable(false);
-        textBox.setWrapText(true);
-        textBox.setPadding(new Insets(10));
-        textBox.setFont(Font.font(18));
-        textBox.setPrefHeight(162);
-        return textBox;
-    }
+
+//    private TextArea createTextArea() {
+//        TextArea textBox = new TextArea();
+//        textBox.setEditable(false);
+//        textBox.setWrapText(true);
+//        textBox.setPadding(new Insets(10));
+//        textBox.setFont(Font.font(18));
+//        textBox.setPrefHeight(162);
+//        return textBox;
+//    }
+
   
-    private void handleNextText(Stage primaryStage, TextArea textBox) {
+    private void handleNextText(Stage primaryStage, TextFlow textBox) {
         if (currentTextIndex < storyTexts.length - 1) {
             currentTextIndex++;
-            textBox.clear();
+            textBox.getChildren().clear();
             updateSpeakerVisibility();
-            playEffectSound(storyTexts[currentTextIndex][2]); // เล่นเสียงเอฟเฟกต์
+            playEffectSound(storyTexts[currentTextIndex][3]); // เล่นเสียงเอฟเฟกต์
+         // อัปเดตรูปภาพของตัวละครที่กำลังพูด
+            updateCharacterImages();
+            
             timeline.stop();
             timeline = createTimeline(textBox);
             timeline.play();
@@ -146,19 +167,34 @@ public class Chapter1 {
             showNextScene(primaryStage);
         }
     }
-    
+    private void updateCharacterImages() {
+        String currentSpeaker = storyTexts[currentTextIndex][1];
+        String emotion = storyTexts[currentTextIndex][0];
+
+        if (currentSpeaker.equals("คเชน")) {
+            cashenImage.setImage(new Image(getClass().getResource(getImagePath("คเชน", emotion)).toExternalForm()));
+        } else {
+            friendImage.setImage(new Image(getClass().getResource(getImagePath("เพื่อน", emotion)).toExternalForm()));
+        }
+    }
+    private String getImagePath(String speaker, String emotion) {
+        if (speaker.equals("คเชน")) {
+            if (emotion.equals("normal")) return "/resources/cashen_normal.png";
+            if (emotion.equals("smile")) return "/resources/cashen_smile.png";
+        } else if (speaker.equals("เพื่อน")) {
+            return "/resources/friend_normal.png";
+        }
+        return "/resources/default.png"; // กรณีผิดพลาด ให้ใช้ภาพ default
+    }
     private void playEffectSound(String effect) {
         if (effectPlayer != null) {
             effectPlayer.stop(); // หยุดเสียงเก่าก่อนเล่นใหม่
         }
-        String effectPath = null;
-        if( effect == "whoosh") {
-        	 effectPath = "/resources/" + effect + ".mp3"; // เช่น "whoosh.mp3" หรือ "pop.mp3"
-        }else {
-        	 effectPath = "/resources/" + effect + ".mp4"; 
-        }
+        String effectPath = "/resources/" + effect + ".mp3"; 
+        
         
         URL effectURL = getClass().getResource(effectPath);
+
         if (effectURL != null) {
             effectPlayer = new MediaPlayer(new Media(effectURL.toExternalForm()));
             effectPlayer.play();
@@ -167,14 +203,21 @@ public class Chapter1 {
         }
     }
    
+
     private ImageView createSpeakerImage(String speaker) {
-        String imagePath = (speaker.equals("คเชน") ? "/resources/cashen_normal.jpeg" : "/resources/friend.jpeg");
-        ImageView img = createImageView(imagePath, 200, 300);
+        String imagePath = (speaker.equals("คเชน") ? "/resources/cashen_normal.png" : "/resources/friend_normal.png");
+        ImageView  img;
+        if( speaker.equals("เพื่อน")) {
+        	img = createImageView(imagePath, 260, 300);
+        }else {
+        	img = createImageView(imagePath, 200, 300);
+        }
+        
         return img;
     }
-    
+
     private void updateSpeakerVisibility() {
-        String currentSpeaker = storyTexts[currentTextIndex][0];
+        String currentSpeaker = storyTexts[currentTextIndex][1];
         if (currentSpeaker.equals("คเชน")) {
             cashenImage.setOpacity(1.0);
             friendImage.setOpacity(0.8);
@@ -183,21 +226,35 @@ public class Chapter1 {
             friendImage.setOpacity(1.0);
         }
     }
-    
-    private Timeline createTimeline(TextArea textBox) {
-        String currentSpeaker = storyTexts[currentTextIndex][0];
-        String currentText = storyTexts[currentTextIndex][1];
+
+    private Timeline createTimeline(TextFlow textBox) {
+        String currentSpeaker = storyTexts[currentTextIndex][1];
+        String currentText = storyTexts[currentTextIndex][2];
+
         Timeline timeline = new Timeline();
-        textBox.appendText(currentSpeaker + " : ");
+        textBox.getChildren().clear(); // เคลียร์ข้อความเก่าก่อนเริ่มใหม่
+
+        // ทำให้ชื่อผู้พูดดูเด่น
+        Text speakerText = new Text(currentSpeaker + " \n");
+        
+        speakerText.setFill(Color.RED); // เปลี่ยนสีเป็นแดงให้ดูเด่น
+        speakerText.setFont(Font.loadFont(getClass().getResourceAsStream("/resources/Prompt-ExtraLight.ttf"), 20));
+        // ข้อความที่พิมพ์ทีละตัว
+        Text contentText = new Text();
+        
+        contentText.setFont(Font.loadFont(getClass().getResourceAsStream("/resources/Prompt-ExtraLight.ttf"), 18));
+        textBox.getChildren().addAll(speakerText, contentText); // ใส่ลงใน TextFlow
+        
         for (int i = 0; i < currentText.length(); i++) {
             final int index = i;
             timeline.getKeyFrames().add(new KeyFrame(Duration.millis(33 * (i + 1)), e -> {
-                textBox.appendText(String.valueOf(currentText.charAt(index)));
+                contentText.setText(contentText.getText() + currentText.charAt(index)); // เพิ่มตัวอักษรทีละตัว
             }));
         }
+
         return timeline;
     }
-    
+
     private void showNextScene(Stage primaryStage) {
     	 if (backgroundMusic != null) {
     	        backgroundMusic.stop(); // หยุดเสียงก่อนเปลี่ยนฉาก
@@ -207,3 +264,5 @@ public class Chapter1 {
         primaryStage.setScene(new Scene(nextSceneRoot, 968, 648));
     }
 }
+
+
